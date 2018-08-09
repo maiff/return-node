e4686ec1fc78b9d969dc73c91b11f939d4065b1e
XiangWangtao <xwt2101239@gmail.com>
2018-08-09 15:14:57 +0800

    modify json

--- save/save.js
+++ save/save.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 module.exports = function (urlJson) {
   return new Promise((resolve, reject) => {
-    fs.writeFile('urlList.json', urlJson, function (err) {
+    fs.writeFile('/app/urlList.json', urlJson, function (err) {
       err && reject(err)
       resolve()
     })
